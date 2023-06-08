013a066ae06d148883d3c41954a5a3baf281f04d
TerryW624 <terrywade634@gmail.com>
2023-06-08 13:22:57 -0700

    feat: add all ingredient from order to ingredients

--- Food.py
+++ Food.py
@@ -1,24 +1,26 @@
 class Order:
-    def __init__(self):
-        self.name = None
+    def __init__(self, dish_name, price):
+        self.dish_name = dish_name
+        self.price = price
         self.ingredients = []
+        
+    def add_ingredients(self, toppings, cheese):
+        self.ingredients += toppings
+        self.ingredients += cheese
+        return self.ingredients
 
 class Pizza(Order):
-    def __init__(self):
-        super().__init__()
-        self.name = "Pizza"
-        self.cheese = None
-        self.toppings = None
-        self.ingredients = ["Dough", "Tomato Sauce", self.cheese, self.toppings]
+    def __init__(self, dish_name, price, toppings, cheese):
+        super().__init__(dish_name, price)
+        self.cheese = cheese
+        self.toppings = toppings
+        self.ingredients = ["Dough", "Tomato Sauce"]
 
 class Pasta(Order):
-    def __init__(self, sauce, pasta):
-        super().__init__()
-        self.sauce = sauce
-        self.pasta = pasta
-        self.name = "Pasta"
+    def __init__(self, dish_name, price):
+        super().__init__(dish_name, price)
+        self.ingredients = []
 
 class Salad(Order):
-    def __init__(self):
-        super().__init__()
-        self.name = "Salad"
+    def __init__(self, dish_name, price):
+        super().__init__(dish_name, price)
